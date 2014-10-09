31ca0d4043ac8c47b0b6ad31de1c084994de4bc0
Joel Sommers <jsommers@colgate.edu>
2014-10-09 17:04:05 -0400

    Modify temp script to reliably get temp

--- temp.py
+++ temp.py
@@ -1,13 +1,11 @@
 #/usr/bin/env python3
 
-import time
 import os
 import glob
 import sys
-import socket
-import threading
 import atexit
 import signal
+import re
 
 def read_temp_raw():
     base_dir = '/sys/bus/w1/devices/'
@@ -15,11 +13,12 @@ def read_temp_raw():
         device_folder = glob.glob(base_dir + '28*')[0]
     except:
         return ''
-    device_file = device_folder + '/w1_slave'
+    # print (device_folder)
+    device_file = os.path.join(device_folder, 'w1_slave')
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
-    return lines
+    return [ s.strip() for s in lines ]
 
 def read_temp():
     lines = read_temp_raw()
@@ -27,14 +26,17 @@ def read_temp():
         # it no workie --- no temperature found
         return 0.0,0.0
 
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
-        lines = read_temp_raw()
-        equals_pos = lines[1].find('t=')
-        if equals_pos != -1:
-            temp_string = lines[1][equals_pos+2:]
-            temp_c = float(temp_string) / 1000.0
-            temp_f = temp_c * 9.0 / 5.0 + 32.0
-            return temp_c, temp_f
+    if not lines[0].endswith('YES'):
+        print ("Device not ready (first line not 'YES')")
+        return 0.0,0.0
+
+    mobj = re.search('t=(\d+)$', lines[1])
+    if not mobj:
+        print ("Can't find end of line stuff")
+        return 0.0,0.0
+
+    temp_c = float(mobj.groups()[0]) / 1000.0
+    temp_f = temp_c * 9.0 / 5.0 + 32.0
+    return temp_c, temp_f
 
 print (read_temp())

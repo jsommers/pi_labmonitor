8969be9ed87c2f0041310fdaf9c110b6dc028179
Joel Sommers <jsommers@colgate.edu>
2015-06-04 12:47:22 -0400

    Add leds to lightstat

--- lightstat.py
+++ lightstat.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python3
- 
-# Example for RC timing reading for Raspberry Pi
-# Must be used with GPIO 0.3.1a or later - earlier verions
-# are not fast enough!
- 
+
 import math, time, os
 import RPi.GPIO as GPIO
- 
+
+GREEN = 18
+RED = 23
+BLUE = 25
+lightpins = ( GREEN, RED, BLUE )
 DEBUG = 1
+
 GPIO.setmode(GPIO.BCM)
- 
+
+for pin in lightpins:
+    GPIO.setup(pin, GPIO.OUT)
+
+for pin in lightpins:
+    GPIO.output(pin, False)
+
 def RCtime (RCpin):
-        reading = 0
-        GPIO.setup(RCpin, GPIO.OUT)
-        GPIO.output(RCpin, GPIO.LOW)
-        time.sleep(0.1)
+    reading = 0
+    GPIO.setup(RCpin, GPIO.OUT)
+    GPIO.output(RCpin, GPIO.LOW)
+    time.sleep(0.1)
  
-        GPIO.setup(RCpin, GPIO.IN)
-        # This takes about 1 millisecond per loop cycle
-        while (GPIO.input(RCpin) == GPIO.LOW):
-                reading += 1
-        return reading
+    GPIO.setup(RCpin, GPIO.IN)
+    # This takes about 1 millisecond per loop cycle
+    while (GPIO.input(RCpin) == GPIO.LOW):
+        reading += 1
+    return reading
 
  
-lvals = [ RCtime(22) for _ in range(25) ]
+for pin in lightpins:
+    GPIO.output(pin, True)
+
+lvals = [ RCtime(22) for _ in range(3) ]
+
+time.sleep(1.0)
+
+for pin in lightpins:
+    GPIO.output(pin, False)
+
 GPIO.cleanup()
 
 n = float(len(lvals))
 lavg = sum(lvals) / n
 var = (( sum([x**2 for x in lvals]) * n ) - ( sum(lvals) ** 2.0 )) / (n * (n - 1))
 
-threshold = 3000
+threshold = 500
 
 status = "off"
-if lavg >= threshold:
+if lavg <= threshold:
     status = "on"
 
 print ("Lights: likely {} (average reading {:.1f}, sdev {:.1f})".format(status, lavg, math.sqrt(var)))
---
+++ lightstatold.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+ 
+# Example for RC timing reading for Raspberry Pi
+# Must be used with GPIO 0.3.1a or later - earlier verions
+# are not fast enough!
+ 
+import math, time, os
+import RPi.GPIO as GPIO
+ 
+DEBUG = 1
+GPIO.setmode(GPIO.BCM)
+ 
+def RCtime (RCpin):
+        reading = 0
+        GPIO.setup(RCpin, GPIO.OUT)
+        GPIO.output(RCpin, GPIO.LOW)
+        time.sleep(0.1)
+ 
+        GPIO.setup(RCpin, GPIO.IN)
+        # This takes about 1 millisecond per loop cycle
+        while (GPIO.input(RCpin) == GPIO.LOW):
+                reading += 1
+        return reading
+
+ 
+lvals = [ RCtime(22) for _ in range(3) ]
+GPIO.cleanup()
+
+n = float(len(lvals))
+lavg = sum(lvals) / n
+var = (( sum([x**2 for x in lvals]) * n ) - ( sum(lvals) ** 2.0 )) / (n * (n - 1))
+
+threshold = 500
+
+status = "off"
+if lavg <= threshold:
+    status = "on"
+
+print ("Lights: likely {} (average reading {:.1f}, sdev {:.1f})".format(status, lavg, math.sqrt(var)))
